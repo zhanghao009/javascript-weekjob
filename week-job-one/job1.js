ffd7d89d948375bb96bdf7f41a48a69267df07ec
zhanghao <zhanghao_web@163.com>
2021-07-13 13:36:07 +0800

    第一周第一节作业

---
+++ week-job-one/job1.js
@@ -0,0 +1,35 @@
+/**
+ * 十进制转64进制
+ * @param {*} number 
+ * @returns 
+ */
+export function string10to64(number) {
+    var chars = '0123456789abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ-~'.split(''),
+        radix = chars.length,
+        qutient = +number,
+        arr = [];
+    do {
+        mod = qutient % radix;
+        qutient = (qutient - mod) / radix;
+        arr.unshift(chars[mod]);
+    } while (qutient);
+    return arr.join('');
+};
+
+/**
+ * 64进制转10进制
+ * @param {*} number_code 
+ * @returns 
+ */
+export function string64to10(number_code) {
+    var chars = '0123456789abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ-~',
+        radix = chars.length,
+        number_code = String(number_code),
+        len = number_code.length,
+        i = 0,
+        origin_number = 0;
+    while (i < len) {
+        origin_number += Math.pow(radix, i++) * chars.indexOf(number_code.charAt(len - i) || 0);
+    }
+    return origin_number;
+};
